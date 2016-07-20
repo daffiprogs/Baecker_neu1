2262d8918c047958f8d75c558d990fdfb15bcd0b
daffiprogs <urlaubkcs@gmx.de>
2016-07-20 12:13:24 +0200

    Änderungen: Löschen und ändern der Anzahl

--- Baecker_neu1/Bestellung_neu1.vb
+++ Baecker_neu1/Bestellung_neu1.vb
@@ -29,19 +29,21 @@
         ' also auch die Zellen sind 0-indiziert
         lastZe = UBound(oSheet.data)
         Dim i As Integer = 0
-
+        'Anzeige der Gruppen
         For i = 1 To lastZe
             oCell = oSheet.getCellByPosition(0, i)
             Gruppe.Items.Add(oCell.string)
         Next
     End Sub
+
+    'Das folgende Sub wird aufgerufen, sobald eine Gruppe durch anklicken gewählt wurde
     Private Sub Gruppe_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Gruppe.SelectedIndexChanged
         SelectGruppe = Gruppe.SelectedItem 'Select Gruppe bekommt die ausgewählte Gruppe zugewiesen
         LB_Artikel.Items.Clear()
         Dim lastZe As String = 0
         oSheet = oDoc.getSheets().getByIndex(1)
         lastZe = UBound(oSheet.data)
-
+        'Anzeige der Artikel
         Dim i As Integer = 1
         For i = 1 To lastZe
             oCell = oSheet.getCellByPosition(0, i)
@@ -57,7 +59,8 @@
         oSheet = oDoc.getSheets().getByIndex(1)
         lastZe = UBound(oSheet.data)
 
-
+        'Wenn die ausgewählte Gruppe und der gewählte Artikel stimmen,
+        'werden Beschreibung und Preis angezeigt. Anzahl wird zurückgesetzt.
         Dim i As Integer = 1
         For i = 1 To lastZe
             oCell = oSheet.getCellByPosition(0, i)
@@ -65,12 +68,11 @@
                 oCell = oSheet.getCellByPosition(1, i)
                 If oCell.string = SelectArtikel Then
                     WahlArtikel = oCell.String
-                    'MessageBox.Show(SelectArtikel)
                     oCell = oSheet.getCellByPosition(2, i)
                     TB_Beschreibung.Text = oCell.string
                     oCell = oSheet.getCellByPosition(4, i)
                     TB_Preis.Text = oCell.string
-                    WahlAnzahl = 1
+                    WahlAnzahl = 0
 
                     Exit For
                 End If
@@ -84,12 +86,28 @@
         WahlAnzahl = NUD_Anzahl.Value
         WahlPreis = WahlAnzahl * WahlPreisOriginal
     End Sub
+
     Private Sub Uebernehmen_Click(sender As Object, e As EventArgs) Handles Uebernehmen.Click
-        lvwAddItem(ListView1, WahlArtikel, WahlAnzahl, WahlPreis)
+        'Prüfen, ob item schon existiert.
+        Dim item1 As ListViewItem = ListView1.FindItemWithText(WahlArtikel)
+        If (item1 IsNot Nothing) Then
+            Dim i = item1.Index
+            If WahlAnzahl = 0 Then
+                ListView1.Items.RemoveAt(i)
+                'Ansonsten, wenn Item exisitiert, Anzahl und Preis ändern
+            Else
+                'Anzahl und Preis ändern
+                ListView1.Items(i).SubItems(1).Text = WahlAnzahl
+                ListView1.Items(i).SubItems(2).Text = WahlPreis
+                i = Nothing
+                NUD_Anzahl.Value = 0
+            End If
+        Else
+            lvwAddItem(ListView1, WahlArtikel, WahlAnzahl, WahlPreis)
+        End If
     End Sub
 
     Public Sub lvwAddItem(ByVal lvw As ListView, ByVal ParamArray Werte() As String)
-
         With lvw.Items
             ' 1. Eintrag hinzufügen - Werte(0) gibt den Namen ListView1 an
             With .Add(Werte(0))
@@ -99,6 +117,7 @@
                     ' Es wird ein String-Array angelegt. werte.lenth-1 gibt an, wieviele Werte
                     Dim SubItems(Werte.Length - 1) As String
                     Array.Copy(Werte, 1, SubItems, 0, Werte.Length - 1)
+
 
                     ' ... und diese via AddRange dem ListView hinzufügen
                     .SubItems.AddRange(SubItems)
